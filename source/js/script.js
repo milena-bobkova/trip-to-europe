7e5a8044e9c4c99bb1310927e6cf92301bd77a16
Milena <bobkova.milena25@gmail.com>
2020-08-17 16:26:19 +0300

    Добавляет реализацию слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,55 @@
-var slider = document.querySelector('.slider');
-var sliderContainer = document.querySelector('.slider__container');
-var toggles = Array.from(document.querySelectorAll('.slider__toggle'));
-var controls = Array.from(document.querySelectorAll('.slider__control'));
-var slides = Array.from(document.querySelectorAll('.slider__item'));
+var HIDDEN = 'hidden';
+var SLIDE = 'slider__item';
+var TOGGLE = 'slider__toggle';
 
-slider.classList.remove('slider--nojs');
+var slideIdToShow = 1;
 
-num = 0;
-slides[num].classList.add('slider__item--show');
+var startSlider = function () {
+  var slides = getElementsHandler(SLIDE);
+  var toggles = getElementsHandler(TOGGLE);
+
+  toggles.forEach(function (toggleNode) {
+    var toggleNodeId = extractIdHandler(toggleNode, TOGGLE);
+    toggleNode.addEventListener('click', function () {
+      showSlideHandler(slides, toggleNodeId)
+    });
+  });
+  showSlideHandler(slides, slideIdToShow);
+}
+
+var getElementsHandler = function (type) {
+  return [].slice.call(document.querySelectorAll('.' + type)).sort(function (slideNodeFirst, slideNodeSecond) {
+    var slideNumFirst = extractIdHandler(slideNodeFirst, SLIDE);
+    var slideNumSecond = extractIdHandler(slideNodeSecond, SLIDE);
+
+    return slideNumFirst > slideNumSecond;
+  });
+}
+
+var extractIdHandler = function (slideNode, baseClass) {
+  var currentClassIndex = slideNode.classList.length;
+  while (currentClassIndex--) {
+    var currentClass = slideNode.classList.item(currentClassIndex);
+    var possibleIdNum = parseInt(currentClass.split('-')[1]);
+    if (isNaN(possibleIdNum)) {
+      continue
+    }
+    var classStringToCheck = baseClass + '-' + possibleIdNum;
+    if (classStringToCheck === currentClass) {
+      return possibleIdNum;
+    }
+  }
+}
+
+var showSlideHandler = function (slides, slideId) {
+  slides.forEach(function (slideNode) {
+    var currentSlideNodeId = extractIdHandler(slideNode, SLIDE);
+    if (currentSlideNodeId === slideId) {
+      slideNode.classList.remove(HIDDEN);
+    } else {
+      slideNode.classList.add(HIDDEN);
+    }
+  });
+}
+
+startSlider();
